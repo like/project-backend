cef33d511fa42ef8138dd74f26f83d49da187416
Lucas Barrena <lucasbarrena98@gmail.com>
2024-03-27 14:51:29 -0300

    Reuse almost everything (#6)

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 require('dotenv').config()
-require('express-async-errors')
 
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const logHTTP = require('tiny-log-http')
-const ErrorHTTP = require('tiny-error-http')
 const Backend = require('like-backend')
+const express = require('like-express')
+const ErrorHTTP = require('tiny-error-http')
 
 module.exports = Backend.launch(main)
 
 async function main () {
   const app = express()
-
-  app.set('trust proxy', true)
-  app.set('json spaces', 2)
-
-  app.disable('x-powered-by')
-  app.disable('etag')
-
-  app.use(cors({ maxAge: 600, credentials: true, origin: '*' }))
-  app.use(express.text())
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
-  app.use(cookieParser())
-  app.use(logHTTP({ userAgent: !Backend.testing }))
 
   app.use(function (req, res, next) {
     // Pass state here like req.db = db
@@ -38,11 +21,8 @@ async function main () {
   app.use('/v1', api)
   app.use(ErrorHTTP.middleware)
 
-  const PORT = process.env.PORT || 1337
-  const HOST = process.env.HOST || '127.0.0.1'
-
   return new Backend({
-    server: app.listen(Backend.testing ? 0 : PORT, HOST),
+    app,
     goodbye: async function () {
       // Close resources here
     }
